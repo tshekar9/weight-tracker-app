d5202ec37ce82c30feb6ef22f877a963d2218d25
Tejaswini Shekar <tej@Tejs-MacBook-Air.local>
2025-07-14 13:27:10 +0530

    Fix: increased password_hash field length

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ login_manager.init_app(app)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password_hash = db.Column(db.String(150), nullable=False)
+    username = db.Column(db.String(512), unique=True, nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
